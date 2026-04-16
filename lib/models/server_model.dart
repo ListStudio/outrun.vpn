95ed2eaceedcc4f63acde4340d6d465e598c749e
BoostLab <tihiyokean1488@gmail.com>
2026-04-16 17:45:34 +0300

    Create Server class in server_model.dart file

---
+++ lib/models/server_model.dart
@@ -0,0 +1,9 @@
+class Server {
+  String name;
+  String country;
+  String protocol;
+  int ping;
+  String subscription;
+
+  Server({required this.name, required this.country, required this.protocol, required this.ping, required this.subscription});
+}
